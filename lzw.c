e30590c6aad14f77f47fd0fcf874dafa1e766342
Anders <as505soreide@gmail.com>
2025-08-09 05:05:52 +0200

    created function for calculating number of bits for initial LZW code size

--- lzw.c
+++ lzw.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <assert.h>
 
+// Lenght of generated datastream, used for development/debugging
+#define LENGHT 100
+#define COLOR_TABLE_SIZE 5
+// Debug file for LZW to write to
+#define DEBUG_OUTPUT_STREAM "LZW_debug_out.txt"
+
+
+
 /*
 Gif files compress image data by using a variant of LZW compression.
 Instead of storing RBG values, Gif files are encoded with indexes into a color table. 
@@ -14,7 +22,7 @@ The variant LZW algorithm can then be used to compress this stream if indexes in
     CTsize is the amount of colors in the color table
     Len is the number of indexes to generate
     
-    The returned array must be freed by the consumer!
+    ! The returned array must be freed by the consumer !
 */
 unsigned char* create_random_index_stream(unsigned char CTsize, unsigned short len, unsigned int seed){
     // Set seed
@@ -34,17 +42,52 @@ unsigned char* create_random_index_stream(unsigned char CTsize, unsigned short l
     return index_stream;
 }; 
 
-#define LENGHT 100
+/*
+    Based on the number of entries in the color table, get the initial LZW minimum code size value
+*/
+unsigned char compute_LZW_minimum_code_size(unsigned char CTsize){
+
+    // Minimum code size is 2 bits, even for black and white gifs with only 2 colors
+    unsigned char num_bits;
+    // Left-bitshift 1 to get largest possible number with 'N' bits until we have enough data for 'M' colors
+    for(num_bits = 2; (1<<num_bits) < CTsize; num_bits++);
+
+    return num_bits;
+}
+
+
+int LZW_compress(FILE* output_stream, unsigned char *input_stream, int num_chars){
+
+}
+
 
 main(){
 
-    unsigned char* uncompressed_stream = create_random_index_stream(5, LENGHT, 404);
+    unsigned char* uncompressed_stream = create_random_index_stream(COLOR_TABLE_SIZE, LENGHT, 404);
     
     printf("Uncompressed stream:\n");
     for(int i = 0; i < LENGHT; i++){
         printf("%d", uncompressed_stream[i]);
+        // Print formating
+        if((i+1) % 16 == 0){
+            printf("\n");
+        }
     }
     printf("\n");
+
+    compute_LZW_minimum_code_size(4);
+    compute_LZW_minimum_code_size(8);
+    compute_LZW_minimum_code_size(16);
+    compute_LZW_minimum_code_size(32);
+    compute_LZW_minimum_code_size(64);
+    compute_LZW_minimum_code_size(128);
+    compute_LZW_minimum_code_size(256);
+
+    // Create debugg output stream for LZW output
+    // Replaced with gif file pointer from gif.c 
+    //FILE *d_fp = fopen(DEBUG_OUTPUT_STREAM, "w");
+
+
 
     // Free allocated memmory
     free(uncompressed_stream);
